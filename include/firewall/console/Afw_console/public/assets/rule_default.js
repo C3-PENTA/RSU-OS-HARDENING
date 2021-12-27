f682246bcab09089f3cc868eb55ca1761a4caed0
YunKeon112 <kyun@pentasecurity.com>
2021-12-28 00:54:40 +0900

    Init rule_default

---
+++ include/firewall/console/Afw_console/public/assets/rule_default.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	// 등록
+	$(".action_register").click(function(){
+		$.register();
+	});
+
+
+	// apply rule
+	$(".action_apply_rule").click(function(){
+		$.apply_rule();
+	});
+
+});

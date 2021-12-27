76884005f8a09517e0431a397778592af29ef124
YunKeon112 <kyun@pentasecurity.com>
2021-12-28 00:23:32 +0900

    Update moduleLoader

--- include/firewall/console/Afw_console/classes/moduleLoader.php
+++ include/firewall/console/Afw_console/classes/moduleLoader.php
@@ -1,3 +1,11 @@
 <?php
 
 Namespace classes\system\framework;
+
+class moduleLoader
+{
+    public function_construct()
+
+    $dir_list = $this->getDirList( ROOT_PATH . "module" );
+    $this->module_name = $this->setModuleList( $dir_list );
+}

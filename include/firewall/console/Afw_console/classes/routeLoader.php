2548679ed5051871ad13701a7ec2c09f8f984a77
YunKeon112 <kyun@pentasecurity.com>
2021-12-28 00:26:21 +0900

    add routeLoader function

--- include/firewall/console/Afw_console/classes/routeLoader.php
+++ include/firewall/console/Afw_console/classes/routeLoader.php
@@ -1,3 +1,14 @@
 <?php
 
 Namespace classes\system\framework;
+
+class routeLoader
+{
+    public function __construct(  )
+    {
+        # 디렉토리 리스트 가져오기.
+        $dir_list = getDir( ROOT_PATH . "module" );
+        # 라우트 등록하기
+        $this->route = $this->setRouting( $dir_list );
+    }
+}

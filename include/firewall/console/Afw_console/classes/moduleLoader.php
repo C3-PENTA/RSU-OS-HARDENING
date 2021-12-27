59360d2cbcf592d7fa22013804cb743249eeacdc
YunKeon112 <kyun@pentasecurity.com>
2021-12-28 00:24:06 +0900

    Get directory list in module

--- include/firewall/console/Afw_console/classes/moduleLoader.php
+++ include/firewall/console/Afw_console/classes/moduleLoader.php
@@ -8,4 +8,10 @@ class moduleLoader
 
     $dir_list = $this->getDirList( ROOT_PATH . "module" );
     $this->module_name = $this->setModuleList( $dir_list );
+
+    private function getDirList ( $path )
+    {
+        $module = getDir( $path );
+        return $module;
+    }
 }

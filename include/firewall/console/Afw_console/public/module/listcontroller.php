4cca4830281908832309f3240a998051a033079c
YunKeon112 <kyun@pentasecurity.com>
2021-12-28 14:41:34 +0900

    Init listcontroller

---
+++ include/firewall/console/Afw_console/public/module/listcontroller.php
@@ -0,0 +1,20 @@
+<?php
+
+use Exception;
+
+class listController
+{
+    public function registerAction()
+	{
+
+
+		$val = array(
+//			"payload_type" => $payload_type
+		);
+
+		echo only_contents($val);
+		die();
+	
+	}
+
+}

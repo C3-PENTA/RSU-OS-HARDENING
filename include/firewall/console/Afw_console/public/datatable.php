bed51a6fa128aaa649a7f1499f4c7db9fcb693a5
YunKeon112 <kyun@pentasecurity.com>
2021-12-28 14:51:48 +0900

    Update datatable

--- include/firewall/console/Afw_console/public/datatable.php
+++ include/firewall/console/Afw_console/public/datatable.php
@@ -5,9 +5,13 @@ trait datatable
     public function dt_init($post)
     {
         $draw   = (isset($post["draw"]))?       $post["draw"]:"";
+        $start  = (isset($post["start"]))?      $post["start"]:"";
+        $length = (isset($post["length"]))?     $post["length"]:"";
 
         $val = array(
             "draw"     => $draw
+            ,"start"    => $start
+            ,"length"   => $length
         );
         return $val;
     }

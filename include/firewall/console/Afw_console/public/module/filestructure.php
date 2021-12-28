e24d5be13bb62ed4580b62c693b2b61344c3ac32
YunKeon112 <kyun@pentasecurity.com>
2021-12-28 14:40:12 +0900

    Update directory listing

--- include/firewall/console/Afw_console/public/module/filestructure.php
+++ include/firewall/console/Afw_console/public/module/filestructure.php
@@ -7,4 +7,16 @@ class fileStructure
 		$list = $this->getLs(ROOT_PATH);
 		debug($list);
 	}
+
+    private function getLs($path){
+		try
+		{
+			if(0){
+				$list = scandir($path);	
+			}else{
+				throw new Exception('test');	
+			}
+		} catch(Exception $e) { dkException($e); }
+			
+	}
 }
